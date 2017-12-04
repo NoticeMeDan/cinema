0c69685c7691fa8f096e11426816badb14bbd50a
Elias Jørgensen <eliaszakarias@gmail.com>
2017-12-04 11:20:00 +0100

    Add test DAO using JDBI

--- src/main/java/com/noticemedan/cinema/Application.java
+++ src/main/java/com/noticemedan/cinema/Application.java
@@ -1,8 +1,22 @@
 package com.noticemedan.cinema;
 
+import com.noticemedan.cinema.dao.TestDao;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.h2.H2DatabasePlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
 
+        Jdbi jdbi = Jdbi.create("jdbc:h2:~/cinemadb/cinema;AUTO_SERVER=true");
+        jdbi.installPlugin(new H2DatabasePlugin());
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        TestDao dao = jdbi.onDemand(TestDao.class);
+
         System.out.println("Hey");
+        System.out.println(Arrays.deepToString(dao.listTests().toArray()));
     }
 }
