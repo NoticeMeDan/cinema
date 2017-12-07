3854a3411c25aa5f26dfb48b4d4d480f9b070bb4
Mathias Egekvist <megekvist@me.com>
2017-12-07 18:19:04 +0100

    inconsistence naming and wrong datatype

--- src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
+++ src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
@@ -1,5 +1,7 @@
 package com.noticemedan.cinema.dao;
 
+import com.noticemedan.cinema.entity.CustomerEntity;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
@@ -8,5 +10,7 @@ public interface CustomerDao {
     @SqlUpdate("INSERT INTO customers(phone_number, orders) VALUES (:phone_number)")
     void saveCustomer(@Bind("phone_number") String phone_number);
 
-    @SqlQuery("SELECT ")
+    @SqlQuery("SELECT customer FROM customers WHERE :phone_number = phone_number")
+    @RegisterBeanMapper(CustomerEntity.class)
+    CustomerEntity getCustomer(@Bind("phone_number") String phone_number);
 }
