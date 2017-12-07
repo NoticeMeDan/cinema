b0d4714bcfc57f3e055262012c2eaf17fd955183
Mathias Egekvist <megekvist@me.com>
2017-12-07 17:04:26 +0100

    unnecessary attributes

--- src/main/java/com/noticemedan/cinema/controller/CustomerController.java
+++ src/main/java/com/noticemedan/cinema/controller/CustomerController.java
@@ -1,11 +1,5 @@
 package com.noticemedan.cinema.controller;
 
-import com.noticemedan.cinema.service.CustomerService;
-
 public class CustomerController {
-    private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    public CustomerController() { }
 }
