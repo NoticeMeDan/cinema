d1e3a3e4d45a2203bf2142553abe2681b14d6e89
Mathias Egekvist <megekvist@me.com>
2017-12-07 20:33:29 +0100

    added testng to pom

--- src/main/java/com/noticemedan/cinema/Application.java
+++ src/main/java/com/noticemedan/cinema/Application.java
@@ -1,5 +1,6 @@
 package com.noticemedan.cinema;
 
+import com.noticemedan.cinema.service.CustomerService;
 import com.noticemedan.cinema.service.TestService;
 
 import java.util.Arrays;
@@ -8,8 +9,10 @@ public class Application {
     public static void main(String[] args) {
 
         TestService testService = new TestService();
+        CustomerService customerService = new CustomerService();
 
         System.out.println("Hey");
         System.out.println(Arrays.deepToString(testService.getTestsWithEasterEgg().toArray()));
+        //System.out.println(Arrays.deepToString(customerService.getTestsWithEasterEgg().toArray()));
     }
 }
