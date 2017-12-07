3c1bdf5e395cf995baa861a5a3c14016f8628e69
Vilfred <Vilfredsikker@gmail.com>
2017-12-07 17:22:33 +0100

    UIController enabled

--- src/main/java/com/noticemedan/cinema/controller/UIController.java
+++ src/main/java/com/noticemedan/cinema/controller/UIController.java
@@ -1,5 +1,6 @@
 package com.noticemedan.cinema.controller;
 
+<<<<<<< HEAD
 import com.noticemedan.cinema.entity.ShowEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -81,5 +82,100 @@ public class UIController implements Initializable {
 
     //Extra features
     //TODO create tutorial
+=======
+import java.util.List;
+
+public class UIController {
+
+    //Getters
+    /*
+    public String getCustomer(int id){
+        return CustomerService.getCustomer();
+    }
+
+    public List<OrderEntity> getOrders(CustomerEntity customer){
+
+        return CustomerService.getOrders();
+    }
+*/
+    //This will be changed to public Movie getMovies()
+    public void getMovies(){
+        System.out.println();
+        //ShowService.getMovie();
+    }
+
+    public int getAmountOfSeats(){
+        return 5;
+        // return OrderService.getAmountOfSeats();
+    }
+    /*
+    public Order getOrder(){
+        Order.getOrder();
+    }
+    */
+
+    public void getOrder(){
+        System.out.println("This is your order:");
+    }
+
+    //Setters
+    public void addOrder(){
+        System.out.println("New order");
+        // OrderService.addOrder();
+    }
+
+    public void setAmountOfSeats(){
+        System.out.println("You chose seats");
+        //OrderService.setAmountOfSeats(amount);
+    }
+
+    public void isChecked(){
+        System.out.println("Checked");
+    }
+    /*public boolean setIndividualSeat(){
+        boolean isChecked;
+        if(seatsChecked.isSelected()){
+
+        }
+    } */
+
+    public void chooseTimeSlot(){
+        System.out.println("Choose timeslot");
+        // /ShowService.chooseTimeSlot();
+    }
+
+    //Choose
+    public void chooseMovie(){
+        System.out.println("Choose movie");
+        //ShowService.chooseMovie();
+    }
+
+    public void chooseDate(){
+        System.out.println("Choose date");
+        //ShowService.chooseDate();
+    }
+
+    public void chooseSeat(){
+        System.out.println("Here you choose seats");
+        //OrderService.chooseSeat();
+    }
+
+    public void saveSettings(){
+        System.out.println("Save");
+        //OrderService.saveSettings();
+    }
+
+    public void cancelSettings(){
+        System.out.println("Cancel");
+        //OrderService.cancelSettings();
+    }
+
+    public void test(){
+        System.out.println("Hey, se det virker");
+    }
+
+
+
+>>>>>>> UIController enabled
 
 }
