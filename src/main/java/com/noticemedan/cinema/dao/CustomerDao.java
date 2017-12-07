ee5c5d8afe0ad2a6214d65dce5f34711ab369f46
Mathias Egekvist <megekvist@me.com>
2017-12-07 18:33:08 +0100

    Customer dao

--- src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
+++ src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
@@ -1,16 +1,25 @@
 package com.noticemedan.cinema.dao;
 
 import com.noticemedan.cinema.entity.CustomerEntity;
+import com.noticemedan.cinema.entity.OrderEntity;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface CustomerDao {
-    @SqlUpdate("INSERT INTO customers(phone_number, orders) VALUES (:phone_number)")
+    @SqlUpdate("INSERT INTO customers(phone_number) VALUES (:phone_number)")
     void saveCustomer(@Bind("phone_number") String phone_number);
 
-    @SqlQuery("SELECT customer FROM customers WHERE :phone_number = phone_number")
+    @SqlUpdate("INSERT INTO customers(orders) " +
+               "VALUES (:id) " +
+               "WHERE :phone_number = customers.phone_number ")
+    void saveCustomerOrders(@Bind("phone_number") String phone_number, @BindBean OrderEntity orders);
+
+    @SqlQuery("SELECT customer " +
+              "FROM customers " +
+              "WHERE :phone_number = customers.phone_number")
     @RegisterBeanMapper(CustomerEntity.class)
     CustomerEntity getCustomer(@Bind("phone_number") String phone_number);
 }
