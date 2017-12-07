29abca50f7a960e88591ac79e5b37c437ef407c7
Mathias Egekvist <megekvist@me.com>
2017-12-07 18:09:43 +0100

    able to create a customer with zero orders

--- src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
+++ src/main/java/com/noticemedan/cinema/dao/CustomerDao.java
@@ -1,9 +1,12 @@
 package com.noticemedan.cinema.dao;
 
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface CustomerDao {
-    @SqlUpdate("INSERT INTO user(phone_number, orders) VALUES (:phone_number) ")
+    @SqlUpdate("INSERT INTO customers(phone_number, orders) VALUES (:phone_number)")
     void saveCustomer(@Bind("phone_number") String phone_number);
+
+    @SqlQuery("SELECT ")
 }
