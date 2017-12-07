d9e3e717e8ba817814fedca7b48d0a5370d91d3e
VilfredSikker <vilfredsikker@gmail.com>
2017-12-07 12:19:54 +0100

    Starting on Controller for Order

--- src/main/java/com/noticemedan/cinema/controller/OrderController.java
+++ src/main/java/com/noticemedan/cinema/controller/OrderController.java
@@ -1,4 +1,15 @@
 package com.noticemedan.cinema.controller;
 
-public class OrderController {
+import java.io.IOException;
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class OrderController extends VBox{
+
+
 }
+
