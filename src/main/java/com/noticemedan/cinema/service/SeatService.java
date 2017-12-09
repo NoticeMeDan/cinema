81e038c51afb59de5751b139b5b41354c8008c9e
Mathias Egekvist <megekvist@me.com>
2017-12-09 14:15:03 +0100

    constructor for seat-entity

--- src/main/java/com/noticemedan/cinema/service/SeatService.java
+++ src/main/java/com/noticemedan/cinema/service/SeatService.java
@@ -6,12 +6,12 @@ public class SeatService extends BaseService {
     public SeatService() { super(); }
 
     public void bookSeat(int seatNumber, int showId, int orderId) {
-        SeatEntity seatEntity = new SeatEntity();
-        seatDao.bookSeat(seatNumber, showId, orderId);
+        SeatEntity seatEntity = new SeatEntity(showId, orderId, seatNumber);
+        seatDao.bookSeat(seatEntity.getSeatNumber(), seatEntity.getShowId(), seatEntity.getOrderId());
     }
 
     public void deleteSeatBooking(int seatNumber, int orderId) {
-        SeatEntity seatEntity = new SeatEntity();
-        seatDao.deleteSeatBooking(seatNumber, orderId);
+        SeatEntity seatEntity = new SeatEntity(orderId, seatNumber);
+        seatDao.deleteSeatBooking(seatEntity.getSeatNumber(), seatEntity.getOrderId());
     }
 }
