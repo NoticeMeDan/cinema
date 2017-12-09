d31fd712bf4e127689a7184ee1d501892e8ba8e3
Mathias Egekvist <megekvist@me.com>
2017-12-09 13:15:22 +0100

    added dao for seat

---
+++ src/main/java/com/noticemedan/cinema/dao/SeatDao.java
@@ -0,0 +1,11 @@
+package com.noticemedan.cinema.dao;
+
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+public class SeatDao {
+    @SqlUpdate("")
+    void
+
+    @SqlQuery("")
+}
