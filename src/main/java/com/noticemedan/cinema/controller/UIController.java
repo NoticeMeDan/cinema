55fa1b8b412b9517aade1d5de0fe937a1795d3b9
Vilfred <Vilfredsikker@gmail.com>
2017-12-09 14:58:25 +0100

    Intialized data, WIP

--- src/main/java/com/noticemedan/cinema/controller/UIController.java
+++ src/main/java/com/noticemedan/cinema/controller/UIController.java
@@ -1,5 +1,6 @@
 package com.noticemedan.cinema.controller;
 
+import com.noticemedan.cinema.entity.ShowEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -13,15 +14,21 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class UIController implements Initializable {
-    //TopPane
-    @FXML private DatePicker pickDate;
+    //Customer ID and Login
     @FXML private Button login;
     @FXML private TextField customerID;
     @FXML private Label showID;
     @FXML private Label ld;
-    @FXML private Label info;
+    //Pick info
+    @FXML private DatePicker pickDate;
     @FXML private ComboBox pickTime;
     @FXML private ComboBox pickMovie;
+    @FXML private Label info;
+    //TableView
+    @FXML private TableView<ShowDummy> tableView;
+    @FXML private TableColumn<ShowDummy, String> movieCol;
+    @FXML private TableColumn<ShowDummy, String> timeCol;
+    @FXML private TableColumn<ShowDummy, String> dateCol;
 
     //Get movie+time+date and display on info-label
     public void getInfo(){
@@ -33,9 +40,16 @@ public class UIController implements Initializable {
         showID.setText(customerID.getText());
     }
     //Things to initialize
+    //Data for Movies and Time
     private ObservableList<String> movies = FXCollections.observableArrayList("John Hitler", "John Hitler 2");
     private ObservableList<String> time = FXCollections.observableArrayList("8:00", "12:00");
-
+    //Data for TableView
+    private ObservableList<ShowDummy> list = FXCollections.observableArrayList(
+            new ShowDummy("John Hitler", "8:00", "8/4/2017"),
+            new ShowDummy("John Hitler 2", "12:00", "8/5/2017"),
+            new ShowDummy("John Hitler", "12:00", "9/4/2017"),
+            new ShowDummy("Finding Nemo", "12:00", "9/4/2017")
+    );
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //Initialize movies with default value
@@ -47,11 +61,13 @@ public class UIController implements Initializable {
         //Initialize date with default value
         pickDate.setValue(LocalDate.now());
         getInfo();
+        //Initialize TableView with data
+        tableView.setItems(list);
     }
-    //TODO TableView receive data
-
     //TODO create new order method
+    public void newOrder(){
 
+    }
     //TODO create save method
 
     //TODO create delete method
