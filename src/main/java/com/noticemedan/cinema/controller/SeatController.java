fff52e3721ac98975c4eb875f18dd4245e06b5af
Mathias Egekvist <megekvist@me.com>
2017-12-09 14:23:14 +0100

    SeatController

---
+++ src/main/java/com/noticemedan/cinema/controller/SeatController.java
@@ -0,0 +1,19 @@
+package com.noticemedan.cinema.controller;
+
+import com.noticemedan.cinema.service.SeatService;
+
+public class SeatController {
+    private SeatService seatService;
+
+    public SeatController() {
+        this.seatService = new SeatService();
+    }
+
+    public void bookSeat(int seatNumber, int showId, int orderId) {
+        seatService.bookSeat(seatNumber, showId, orderId);
+    }
+
+    public void deleteSeatBooking(int seatNumber, int orderId) {
+        seatService.deleteSeatBooking(seatNumber, orderId);
+    }
+}
