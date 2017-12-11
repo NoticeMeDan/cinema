4a8717b1673c822f3631a718dc0c90711b688c1f
Elias Jørgensen <eliaszakarias@gmail.com>
2017-12-11 13:11:46 +0100

    Initialize view from Application.main

--- src/main/java/com/noticemedan/cinema/Application.java
+++ src/main/java/com/noticemedan/cinema/Application.java
@@ -1,21 +1,10 @@
 package com.noticemedan.cinema;
 
-import com.noticemedan.cinema.entity.OrderEntity;
-import com.noticemedan.cinema.entity.SeatEntity;
-import com.noticemedan.cinema.entity.ShowEntity;
-import com.noticemedan.cinema.service.OrderService;
-import com.noticemedan.cinema.service.SeatService;
-import com.noticemedan.cinema.service.ShowService;
-
-import java.util.List;
-import java.util.Optional;
+import com.noticemedan.cinema.view.InitView;
 
 public class Application {
     public static void main(String[] args) {
-
-        SeatService seatService = new SeatService();
-        List<SeatEntity> seats = seatService.getOrderSeats(1);
-
-        System.out.println("Hey");
+        // Init view
+        InitView.main(null);
     }
 }
