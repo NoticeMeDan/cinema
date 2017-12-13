df64d75ae8c7f95865f8847630cb6dbb7ad5460d
Mathias Egekvist <megekvist@me.com>
2017-12-13 20:39:43 +0100

    ordertest

--- src/test/java/com/noticemedan/cinema/OrderTest.java
+++ src/test/java/com/noticemedan/cinema/OrderTest.java
@@ -1,20 +1,29 @@
 package com.noticemedan.cinema;
 
+import com.noticemedan.cinema.controller.OrderController;
+import com.noticemedan.cinema.entity.OrderEntity;
 import com.noticemedan.cinema.service.BaseService;
-import com.noticemedan.cinema.service.OrderService;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class OrderTest extends BaseService {
-    private OrderService orderService;
+    private  OrderController orderController;
 
     @BeforeMethod
     public void setUp() throws Exception {
-        this.orderService = new OrderService();
+        this.orderController = new OrderController();
     }
 
     @Test
     public void testOrderCreation() throws Exception {
+        int orderId = this.orderController.saveOrder("12345678");
+        this.orderController.deleteOrder(orderId);
+    }
 
+    @Test
+    public void testGetOrders() throws Exception {
+        List<OrderEntity> orders = this.orderController.getOrders("12345678");
     }
 }
