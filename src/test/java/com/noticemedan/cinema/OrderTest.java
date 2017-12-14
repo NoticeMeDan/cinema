f520228a0299c9059432b01e747c5cb60b33878d
Mathias Egekvist <megekvist@me.com>
2017-12-14 21:34:39 +0100

    added little more to test

--- src/test/java/com/noticemedan/cinema/OrderTest.java
+++ src/test/java/com/noticemedan/cinema/OrderTest.java
@@ -3,8 +3,10 @@ package com.noticemedan.cinema;
 import com.noticemedan.cinema.entity.OrderEntity;
 import com.noticemedan.cinema.service.BaseService;
 import com.noticemedan.cinema.service.OrderService;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import java.util.List;
 
@@ -19,11 +21,13 @@ public class OrderTest extends BaseService {
     @Test
     public void testOrderCreation() throws Exception {
         int orderId = this.orderService.saveOrder("12345678");
+        Assert.assertFalse(orderId == 0);
         this.orderService.deleteOrder(orderId);
     }
 
     @Test
     public void testGetOrders() throws Exception {
         List<OrderEntity> orders = this.orderService.getOrders("12345678");
+        Assert.assertNotEquals(orders, null);
     }
 }
