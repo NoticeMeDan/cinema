dabc5a15e4a115292ba50be1d33e17c7bb6f8477
Mathias Egekvist <megekvist@me.com>
2017-12-14 22:08:21 +0100

    removed test entity and added test

--- src/test/java/com/noticemedan/cinema/OrderTest.java
+++ src/test/java/com/noticemedan/cinema/OrderTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderTest extends BaseService {
@@ -22,6 +23,7 @@ public class OrderTest extends BaseService {
     public void testOrderCreation() throws Exception {
         int orderId = this.orderService.saveOrder("12345678");
         Assert.assertFalse(orderId == 0);
+        
         this.orderService.deleteOrder(orderId);
     }
 
@@ -29,5 +31,17 @@ public class OrderTest extends BaseService {
     public void testGetOrders() throws Exception {
         List<OrderEntity> orders = this.orderService.getOrders("12345678");
         Assert.assertNotEquals(orders, null);
+    }
+
+    @Test
+    public void testGetOrderForNotExistingCustomer() throws Exception {
+        List<OrderEntity> orders = new ArrayList<>();
+        try {
+            orders = this.orderService.getOrders("123456789");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(orders.size() == 0);
     }
 }
